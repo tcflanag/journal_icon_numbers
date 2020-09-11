f1da11996b7045d1f8c916625d93624f9ef23836
Chris Flanagan <tiwato@gmail.com>
2020-09-10 21:57:27 -0700

    Update to dynamically generated icons

--- README.md
+++ README.md
@@ -10,7 +10,14 @@ For the single digit numbers, it support an option '0' padded format as well (i.
 To use, just drag correctly named journals to your map. The icon will be automatically selected to match the name.
 ![](example.png)
 
-![](icons/d75.svg)
+The multiple styles seen in the example can be selected from the config menu.  
+
+
+** As of 1.0.3, the icons are now generated dynamically and stored in uploads/journal-icon-numbers. It is advised the users of previous versions go through and edit all their icons to migrate to the new location.
+
+Once I have proper migration tools in place, the old icons will be deleted since they slow down installs.
+
 
 ## Known issues
-* If you rename a icon, you need to re-edit it to get the new icon.
+
+* If you rename a note, you need to re-edit it to get the new icon.
--- src/index.js
+++ src/index.js
@@ -24,9 +24,12 @@ function* label_itr() {
             for (const letter of letters) {
                 const label = order ? number + letter: letter + number;
                 const u_l = letter == letter.toUpperCase()?"upper":"lower";
-                const iconFilename = `modules/journal-icon-numbers/icons/${u_l}/${label}.svg`;
-                //const iconFilename = `modules/journal-icon-numbers/icons/icon.svg?id=${label}`;
-                yield [label,iconFilename];
+                const iconType = game.settings.get('journal-icon-numbers', "iconType");
+                
+                const iconFilename = `${iconType}_${u_l}_${label}.svg`;
+                // TODO Add config for save path (And maybe code for s3?)
+                const iconFilepath = `upload/journal-icon-numbers/${iconFilename}`;
+                yield [label,iconFilepath,iconFilename];
             }
         }
     }
@@ -41,19 +44,44 @@ function renderNoteConfig(app, html, data) {
     
     
     for (const idx of [idx3, idx2, idx1]) {
-        for (const [label,iconFilename] of label_itr()) {
+        for (const [label,iconFilepath,iconFilename] of label_itr()) {
             
             if (idx === label) {
                 console.debug("Auto-Journal-Icon",label,iconFilename);
                 // Iterator returns >10K entries, so only display ones that match this label
 
-                // Fix for Pin Cushion, which uses a file picker instead of the dropdown
-                $('input.icon-path[name="icon"]').val(iconFilename);
-
-                $('select[name="icon"]', html).append(`<option value="${iconFilename}" selected>${label}</option>`);
+                const iconType = game.settings.get('journal-icon-numbers', "iconType");
                 
+                // Fix for Pin Cushion, which uses a file picker instead of the dropdown
+                $('input.icon-path[name="icon"]').val(iconFilepath);
+                
+                // Add item to selector
+                $('select[name="icon"]', html).append(`<option value=${iconFilepath} selected>${label}</option>`);
+                
+                // TODO Add config for this size override
                 $('input[name="iconSize"]').val(Math.round(game.scenes.viewed.data.grid * 0.75));
-
+                
+                
+                // TODO refactor blobs of html
+                var typeString = '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" style="shape-rendering:geometricPrecision; text-rendering:geometricPrecision; image-rendering:optimizeQuality; fill-rule:evenodd; clip-rule:evenodd" viewBox="0 0 512 512" width="512" height="512"><g>';
+                
+                switch (iconType) {
+                    case "wcbf":
+                        typeString += '<circle style="fill:#ffffff;stroke:#010101;stroke-width:30;stroke-miterlimit:10;" cx="250" cy="250" r="220"> </circle> <text font-family=\'-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"\' font-size="200" font-weight="400" fill="black" x="50%" y="52%" text-anchor="middle" stroke="#000000" dy=".3em">';
+                        break
+                    case "ncwf":
+                        typeString += ' <text font-family=\'-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"\' font-size="200" font-weight="400" fill="white" x="50%" y="52%" text-anchor="middle" stroke="#FFFFFF" dy=".3em">';
+                        break;
+                    case "bcwf":
+                        typeString += '<circle style="fill:#010101;stroke:#FFFFFF;stroke-width:30;stroke-miterlimit:10;" cx="250" cy="250" r="220"> </circle> <text font-family=\'-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"\' font-size="200" font-weight="400" fill="white" x="50%" y="52%" text-anchor="middle" stroke="#FFFFFF" dy=".3em">';
+                }
+                
+                let file = new File([typeString  + label + '</text></g></svg>'], iconFilename, { type: data.type });
+                
+                
+                const result = FilePicker.upload("data", "upload/journal-icon-numbers", file, {  });
+                // TODO Error checking 
+               
                 return;
             }
         }        
@@ -65,10 +93,37 @@ function renderNoteConfig(app, html, data) {
 // Need to figure out a better way to do this
 
 Hooks.once("ready", function() {
-  Hooks.on("renderNoteConfig", renderNoteConfig);
+    makeDirs();
+    Hooks.on("renderNoteConfig", renderNoteConfig);
 });
 
+async function makeDirs() {
+  // TODO add checks here to see dirs already exist
+  FilePicker.createDirectory("data","upload/journal-icon-numbers",{});
+};
 
+/**
+ * Hook on init
+ */
+Hooks.on("init", () => {
+    registerSettings();
+});
 
+function registerSettings() {
 
+    game.settings.register('journal-icon-numbers', "iconType", {
+        name: "SETTINGS.IconStyleN",
+        hint: "SETTINGS.IconStyleH",
+        scope: "client",
+        type: String,
+        choices: {
+            wcbf: "Black text on white circle",
+            bcwf: "White text on black circle",
+            ncwf: "White text, no circle"
+        },
+        default: "white_circle_black_font",
+        config: true,
+        onChange: s => {}
+    });
+}
 
